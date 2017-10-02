24f76eaade01e1aedb031f9cd06da611ce1ae487
Jeff Aldinger <jeff.aldinger@gmail.com>
2017-10-02 00:04:05 -0400

    Everything compiles and you can run it in an emulator now, register button actually takes you to the register page

--- app/src/main/java/com/example/cryptocoins/RegisterActivity.java
+++ app/src/main/java/com/example/cryptocoins/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.cryptocoins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final EditText etFirstName = (EditText) findViewById(R.id.etFirstName);
+        final EditText etLastName = (EditText) findViewById(R.id.etLastName);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etConfirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etConfirmEmail = (EditText) findViewById(R.id.etConfirmEmail);
+
+        final Button bSignUp = (Button) findViewById(R.id.bSignUp);
+
     }
 }
--- app/src/main/java/com/example/cryptocoins/UserAreaActivity.java
+++ app/src/main/java/com/example/cryptocoins/UserAreaActivity.java
@@ -2,6 +2,7 @@ package com.example.cryptocoins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UserAreaActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class UserAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+        final TextView tvWelcome = (TextView) findViewById(R.id.tvWelcome);
     }
 }
