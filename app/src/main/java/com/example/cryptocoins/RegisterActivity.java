c3075b35a8f3eed0bc54df3c1aa6bd4cff8c3860
Jeff Aldinger <jeff.aldinger@gmail.com>
2017-10-10 21:50:27 -0400

    Attempting to use a database for registration, not working properly yet

--- app/src/main/java/com/example/cryptocoins/RegisterActivity.java
+++ app/src/main/java/com/example/cryptocoins/RegisterActivity.java
@@ -1,9 +1,21 @@
 package com.example.cryptocoins;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.android.volley.toolbox.Volley.newRequestQueue;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,5 +33,46 @@ public class RegisterActivity extends AppCompatActivity {
 
         final Button bSignUp = (Button) findViewById(R.id.bSignUp);
 
+        bSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String first_name = etFirstName.getText().toString();
+                final String last_name = etLastName.getText().toString();
+                final String email = etEmail.getText().toString();
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response){
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success){
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Registry failed")
+                                        .setNegativeButton("Retry",null)
+                                        .create()
+                                        .show();
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(first_name, last_name, email, username, password, responseListener);
+                RequestQueue queue = newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/cryptocoins/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.example.cryptocoins;
+
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class RegisterRequest extends StringRequest {
+    private static final String REGISTER_REQUEST_URL = "http://crypto-coins.000webhost.com/Register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String first_name, String last_name, String email, String username, String password, Response.Listener<String> listener){
+        super(Method.POST,REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("first_name", first_name);
+        params.put("last_name", last_name);
+        params.put("email", email);
+        params.put("username", username);
+        params.put("password", password);
+
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
