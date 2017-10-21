2c5a6959b1bed627581a01d57c1c610bdae1a522
Jeff Aldinger <jeff.aldinger@gmail.com>
2017-10-21 18:16:29 -0400

    Added functionality to "confirm password" and "confirm email" as well as the checkbox in the registration page

--- app/src/main/java/com/example/cryptocoins/RegisterActivity.java
+++ app/src/main/java/com/example/cryptocoins/RegisterActivity.java
@@ -1,10 +1,12 @@
 package com.example.cryptocoins;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +17,8 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Objects;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -33,6 +37,9 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText etConfirmEmail = (EditText) findViewById(R.id.etConfirmEmail);
         final TextView termsLink = (TextView) findViewById(R.id.tvTerms);
         final Button bSignUp = (Button) findViewById(R.id.bSignUp);
+        final CheckBox cbTerms = (CheckBox) findViewById(R.id.cbTerms);
+
+
 
         termsLink.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -43,29 +50,47 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         bSignUp.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("NewApi")
             @Override
             public void onClick(View v) {
                 final String first_name = etFirstName.getText().toString();
                 final String last_name = etLastName.getText().toString();
                 final String email = etEmail.getText().toString();
+                final String confirmEmail = etConfirmEmail.getText().toString();
                 final String username = etUsername.getText().toString();
                 final String password = etPassword.getText().toString();
+                final String confirmPassword = etConfirmPassword.getText().toString();
+                final AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                String errorString = "";
 
+                if(!Objects.equals(password, confirmPassword)){
+                    errorString+="The passwords don't match.\n";
+                }
+                if(!Objects.equals(email, confirmEmail)){
+                    errorString+="The emails don't match.\n";
+                }
+                if(!cbTerms.isChecked()){
+                    errorString+="You must agree to the terms.\n";
+                }
+                final String finalErrorString = errorString;
                 Response.Listener<String> responseListener = new Response.Listener<String>(){
                     @Override
                     public void onResponse(String response){
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
                             boolean success = jsonResponse.getBoolean("success");
-                            if(success){
-                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                RegisterActivity.this.startActivity(intent);
-                            }else{
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("Registry failed")
+                            if(!success | finalErrorString!=""){
+
+                                builder.setMessage("Registry failed.\n"+ finalErrorString)
                                         .setNegativeButton("Retry",null)
                                         .create()
                                         .show();
+
+
+                            }else{
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+
                             }
 
 
